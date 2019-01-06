e9ad6a95a7d9de92691f0491b0d284b99418e822
Franz Kohnle <franz@kohnlehome.de>
2019-01-06 10:50:14 +0100

    Add Data

--- src/main/java/de/gbsschulen/ormvererbung/App.java
+++ src/main/java/de/gbsschulen/ormvererbung/App.java
@@ -1,5 +1,8 @@
 package de.gbsschulen.ormvererbung;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,6 +15,23 @@ public class App {
 
     public static void main(String[] args) {
         createDB();
+        createData();
+    }
+
+    private static void createData() {
+        Konto konto1 = new Konto();
+        Konto konto2 = new Konto();
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ORMVererbungPU");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(konto1);
+        em.persist(konto2);
+
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
     }
 
     private static void createDB() {
@@ -20,6 +40,9 @@ public class App {
             Statement stmt = connection.createStatement();
             stmt.executeUpdate("DROP DATABASE IF EXISTS " + DB_NAME);
             stmt.executeUpdate("CREATE DATABASE " + DB_NAME);
+
+            stmt.close();
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
